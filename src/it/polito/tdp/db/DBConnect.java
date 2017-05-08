c3efa4a66d5afc30ef46323a44258343c4385abb
Arianna <Arianna@172.20.94.36>
2017-05-08 09:39:39 +0200

    implementazione con connection pooling.

--- src/it/polito/tdp/db/DBConnect.java
+++ src/it/polito/tdp/db/DBConnect.java
@@ -4,14 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.DataSources;
+
 public class DBConnect {
 	
-	private static String jdbcURL = "jdbc:mysql://localhost/countries?user=root&password=root" ;
+	private static String jdbcURL = "jdbc:mysql://localhost/countries?user=root" ;
+	private static DataSource ds;
 	
 	public static Connection getConnection() {
+		if(ds==null){
+			//creo il datasource
+			try {
+				ds = DataSources.pooledDataSource(DataSources.unpooledDataSource(jdbcURL));//da un unpooled a un pooled
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.exit(1); //Esco dal programma perchè non posso fare più nulla
+			} 
+		}
 		
 		try {
-			Connection c = DriverManager.getConnection(jdbcURL) ;
+			Connection c = ds.getConnection() ; //quando acquisisco la connessione dal pool, lui sa già l'URL quindi non serve passarglielo
 			return c ;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
